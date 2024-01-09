3a209f2421c805754621de2f86dc998aadf2d710
du <tb.poopp@gmail.com>
2024-01-10 00:26:43 +0700

    feat: add exercise middleware

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 import assignmentRouter from "./apps/assignments.js";
+import logging from "./milddleware/logging.js";
+import validateAssignmentData from "./milddleware/assignmentValidation.js";
 
 const app = express();
 const port = 4000;
 
 app.use(bodyParser.json());
+app.use([logging, validateAssignmentData]);
 app.use("/assignments", assignmentRouter);
 
 app.listen(port, () => {
